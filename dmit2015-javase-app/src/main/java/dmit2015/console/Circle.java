982f7bf015d54f3b77329c608cfc8ba8321a4c39
Sam Wu <swu@nait.ca>
2026-01-14 17:58:04 -0700

    Added dmit2015-javase-app project.

---
+++ dmit2015-javase-app/src/main/java/dmit2015/console/Circle.java
@@ -0,0 +1,22 @@
+package dmit2015.console;
+
+/**
+ * This class models a Circle shape.
+ *
+ * @author Sam Wu
+ * @version 2026.01.14
+ */
+public class Circle {
+    // Define a field to track the radius of this circle
+    private double radius;
+
+    // Getters/Setters for fields
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
