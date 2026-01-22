cba4a0ab6a2365cb9ab88be5328b87b16d4767d2
Sam Wu <swu@nait.ca>
2026-01-21 18:04:49 -0700

    Added HelloWorld example

--- dmit2015-faces-app/src/main/java/dmit2015/faces/HelloBean.java
+++ dmit2015-faces-app/src/main/java/dmit2015/faces/HelloBean.java
@@ -2,21 +2,38 @@ package dmit2015.faces;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import net.datafaker.Faker;
+import org.omnifaces.util.Messages;
 
-@Named//("helloBean")
+
+@Named("hello")
 @RequestScoped
 public class HelloBean {
 
-    @Getter @Setter
-    private String userInput = "Server Faces";
+    @NotBlank(message = "User input is required.")
+    private String userInput;
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
 
     public String getMessage() {
         return "Hello, " + userInput;
     }
 
-    public void submit() {
+    public String submit() {
 
+        // Create a Faker instance for generating realistic fake data
+        var faker = new Faker();
+        var pokemonName = faker.pokemon().name();
+        var pokemonMove = faker.pokemon().move();
+        Messages.addGlobalInfo("Hi {0}, you got a {1} pokemon that likes to {2}",
+                userInput, pokemonName, pokemonMove);
+        return null; // or navigation outcome
     }
 }
