defed03c3ddb335153c9a87ac8206ef9316382fa
Sam Wu <swu@nait.ca>
2026-01-30 18:34:09 -0700

    Added Faces CRUD page for Task

--- dmit2015-faces-app/src/main/java/dmit2015/model/Task.java
+++ dmit2015-faces-app/src/main/java/dmit2015/model/Task.java
@@ -4,11 +4,19 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.datafaker.Faker;
+
+import java.util.UUID;
+import java.util.random.RandomGenerator;
 
 //@ToString()
 //@Getter @Setter
 @Data   // includes @Getter, @Setter, @ToString, and more
+@NoArgsConstructor
 public class Task {
+
+    private String id;  // unique identifier
 
     @NotBlank(message = "Task description is required")
     @Size(min=3, max=150,
@@ -20,5 +28,30 @@ public class Task {
     private String priority;    // Low, Medium, High
 
     private boolean done;
+
+    public Task(Task other) {
+        this.id = other.getId();
+        this.description = other.getDescription();
+        this.priority = other.getPriority();
+        this.done = other.isDone();
+    }
+
+    public static Task copyOf(Task other) {
+        return new Task(other);
+    }
+
+
+    public static Task of(Faker faker) {
+        Task currentTask = new Task();
+        currentTask.setId(UUID.randomUUID().toString());
+        currentTask.setDescription("Nuke " + faker.fallout().location());
+        // Define a array of possible values
+        String[] possiblePriorities = {"Low","Medium","High"};
+        // Generate an index to pick from the array
+        int randomIndex = RandomGenerator.getDefault().nextInt(0, possiblePriorities.length);
+        currentTask.setPriority(possiblePriorities[randomIndex]);
+        currentTask.setDone(RandomGenerator.getDefault().nextBoolean());
+        return currentTask;
+    }
 
 }
