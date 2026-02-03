53015545a18d05cf9d01930fb31854b14c5b36f4
Sam Wu <swu@nait.ca>
2026-02-02 18:45:15 -0700

    Added FirebaseRTDB implmentation of TaskService.

--- dmit2015-faces-app/src/main/java/dmit2015/faces/TaskCrudView.java
+++ dmit2015-faces-app/src/main/java/dmit2015/faces/TaskCrudView.java
@@ -30,7 +30,8 @@ public class TaskCrudView implements Serializable {
     }
 
     @Inject
-    @Named("memoryTaskService")
+//    @Named("memoryTaskService")
+    @Named("firebaseHttpClientTaskService")
     private TaskService taskService;
 
     /**
