49217d7588f1526a64ce16799281d91e16dff868
Sam Wu <swu@nait.ca>
2026-02-23 18:33:03 -0700

    Added JPA repository for Student entity.

--- dmit2015-jpa-faces-demo/src/main/java/dmit2015/entity/StudentInitializer.java
+++ dmit2015-jpa-faces-demo/src/main/java/dmit2015/entity/StudentInitializer.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.context.Initialized;
 import jakarta.enterprise.event.Observes;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 @ApplicationScoped
 public class StudentInitializer {
@@ -12,6 +13,7 @@ public class StudentInitializer {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Transactional
     public void initialize(@Observes @Initialized(ApplicationScoped.class) Object event) {
 
         long studentCount = entityManager
@@ -20,18 +22,18 @@ public class StudentInitializer {
         if (studentCount == 0) {
             // Create and seed the database with 3 sample records
             var student1 = new Student();
-            student1.setFirstName("First1");
-            student1.setLastName("Last1");
+            student1.setFirstName("Jon");
+            student1.setLastName("Snow");
             entityManager.persist(student1);
 
             var student2 = new Student();
-            student2.setFirstName("First2");
-            student2.setLastName("Last2");
+            student2.setFirstName("Bart");
+            student2.setLastName("GlassFish");
             entityManager.persist(student2);
 
             var student3 = new Student();
-            student3.setFirstName("First3");
-            student3.setLastName("Last3");
+            student3.setFirstName("Amanda");
+            student3.setLastName("Tapping");
             entityManager.persist(student3);
         }
 
---
+++ dmit2015-jpa-faces-demo/src/main/java/dmit2015/repository/StudentRepository.java
@@ -0,0 +1,33 @@
+package dmit2015.repository;
+
+import dmit2015.entity.Student;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@ApplicationScoped
+public class StudentRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void add(Student newStudent) {
+        entityManager.persist(newStudent);
+    }
+
+    public List<Student> findAll() {
+        return entityManager
+                .createQuery("select s from Student s order by s.lastName, s.firstName", Student.class)
+                .getResultList();
+    }
+
+    public long count() {
+        return entityManager
+                .createQuery("select count(s) from Student s", Long.class)
+                .getSingleResult();
+    }
+}
